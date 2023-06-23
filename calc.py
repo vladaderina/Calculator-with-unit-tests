ee4e52fc2a77453c7de164fe3f91e8c750b3a3d4
Alexkolex <137480390+Alexkolex@users.noreply.github.com>
2023-06-23 12:17:20 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,17 +5,18 @@ def calc(expression):
         raise ValueError("Выражение должно содержать оператор")
     sign = re.findall(reg, expression)[0]
     try:
-        first, second = expression.split(sign)
-        first, second = int(first), int(second)
-        dict = {
-            '+': lambda a, b: a + b,
-            '-': lambda a, b: a - b,
-            '/': lambda a, b: a / b,
-            '*': lambda a, b: a * b
-        }
-        return dict[sign](first, second)
+            first, second = expression.split(sign)
+            first, second = int(first), int(second)
+            if sign == "+":
+                return first + second
+            elif sign == "-":
+                return first - second
+            elif sign == "*":
+                return first * second
+            elif sign == "/":
+                return first / second
     except (ValueError, TypeError):
         raise ValueError("Выражение должно содержать только 2 целых числа и 1 знак")
 
-if __name__ == '__main__':
-    print(calc("2+4"))
+    if __name__ == '__main__':
+        print(calc("2+4"))
